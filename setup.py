d2723b1d9735b1bdb38ccadb9e98b8fc0930f495
Luca Pinello <lucapinello@gmail.com>
2019-01-20 11:54:09 -0500

    installation

---
+++ setup.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2018 Luca Pinello
+# Made available under the MIT license.
+
+
+#This is based on: https://github.com/Roastero/freshroastsr700
+
+import os
+from setuptools import setup
+from setuptools import find_packages
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+
+setup(
+    name='create_artisan_alarms_phidget',
+    version=0.1,
+    description='Create artisan profiles using the alarms and the modified version of the freshroastsr700 library for phidget',
+    url='https://github.com/lucapinello/create_artisan_alarms_phidget',
+    author='Luca Pinello',
+    license='GPLv3',
+    packages=find_packages(),
+    install_requires=[
+        'scipy',
+        'numpy',
+        'matplotlib',
+        'PySide2',
+    ],
+    entry_points = {'console_scripts': ['create_artisan_profile=create_artisan_profile:main']}
+    )
